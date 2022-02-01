2a8cffaded1a56b71420e23152389dfaf9628f91
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2022-02-02 01:48:29 +0530

    Navbar Completed

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Navbar from './Navbar';
+
+export default function Home(){
+    return(
+        <>
+            <Navbar/>
+        </>
+    )
+
+} 
