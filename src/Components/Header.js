4cf67eeb79fea9df91e6d10c38e00049c065947c
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2022-02-02 05:46:59 +0530

    Header

---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Header = () => {
+    return (
+        <>
+            <div className="header-wraper">
+                <div className="main-info">
+                    <h1>Hi, I'm Lakshay!</h1>
+                    <div>
+
+
+                        <Typed
+                            className='typed-text'
+                            strings={['Student at NITKKR',
+                                'Web Developer',
+                                'Android App Developer',
+                                'DIP Enthusiast']}
+                            typeSpeed={40}
+                            backSpeed={60}
+                            loop >
+
+                        </Typed>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
