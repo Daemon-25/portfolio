e348061f3fc375c9ae71a0a9bf3ca31fb49fb3ce
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2022-02-02 05:55:14 +0530

    Header finished

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Navbar from './Navbar';
+import Header from './Header';
+
 
 export default function Home(){
     return(
         <>
+            
             <Navbar/>
+            <Header/>
         </>
     )
 
